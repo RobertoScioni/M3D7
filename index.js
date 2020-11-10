41c2ceac07ae7e7a18b7c82ac1923927569d767e
Roberto <72069799+RobertoScioni@users.noreply.github.com>
2020-11-10 15:33:51 +0100

    Ex 2 done

--- index.js
+++ index.js
@@ -6,28 +6,44 @@ const config = {
 	title: "hello,Strive!", ///that as of now means only the title but i belive this object will grow soon
 }
 /**
+ *  this will contain all the users
+ */
+let users = []
+
+/**
  * initialize the page
  *
  */
-
 const loadusers = async () => {
-	try{
-	  const loaddata = await fetch(`https://jsonplaceholder.typicode.com/users `)
-	  users_data = await loaddata.json()
-	  
-	 
-	  return users_data;
+	try {
+		const loaddata = await fetch(`https://jsonplaceholder.typicode.com/users `)
+		const users_data = await loaddata.json()
+		users = users_data
+		return users_data
+	} catch (err) {
+		console.log(err)
 	}
-	  
-	  catch(err){
-		(console.log(err));
-	  }
-   
-	 
-	  
-	  
-  };
-window.onload = () => {
+}
+/**
+ *
+ */
+const handleFilter = (vector) => {
+	console.log("vector", vector)
+	const query = document.querySelector("#filter").value
+	const qTarget = document.querySelector("#qTarget").value
+	return vector.filter((element) => element[qtarget].includes(query))
+	/*console.log(
+		"result",
+		vector.filter((element) => element[qTarget].includes(query))
+	)*/
+}
+
+window.onload = async () => {
 	document.title = config.title
 	document.querySelector("#mainMenu").innerHTML = config.title
+	loadusers()
+
+	document
+		.querySelector("#filter")
+		.addEventListener("keyup", () => handleFilter(users))
 }
